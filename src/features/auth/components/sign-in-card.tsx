fdf7370cd33738e3c48c80c95715c4fe0e3bc8f7
Subodh Katna <subodhkatna@gmai.com>
2025-11-28 03:40:24 +0530

    Completed the frontend part of the auth and install hono

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,126 @@
+import { z } from "zod";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "Email is required" }) // Just checks if field is empty
+    .email({ message: "Please enter a valid email address" }) // Checks format
+    .trim()
+    .toLowerCase(),
+
+  password: z.string().min(1, { message: "Password is required" }), // REMOVED: min length and regex complexity
+});
+
+export const SignInCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="Enter email address"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      placeholder="Enter your password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full" size="lg" disabled={false}>
+              Log-In
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full">
+          <FcGoogle className="mr-2 size-5" />
+          Log-In With Google
+        </Button>
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full">
+          <FaGithub className="mr-2 size-5" />
+          Log-In With Github
+        </Button>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Don&apos;t an account?
+          <Link href="/sign-up">
+              <span className="text-blue-700">&nbsp;Sign-up</span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
